e7ab67e599b92b2c73c73b0ce7a60468214bf8e0
edgardo445 <esaavedra1234556@gmail.com>
2025-08-13 12:25:02 -0700

    Add files via upload

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,18 @@
+    // Created by Edgardo Saavedra on 8/13/2025.
+    // addTwoNumbers.cpp
+#include <iostream>
+using namespace std;
+
+// Create a very important function in main
+// This is where your program starts
+int main() {
+
+}
+    // Create variables needed for this program.
+    int sumOFNums = 0;
+    int num1= 0;
+    int num2= 0;
+    cout << "Enter First Number: ";
+    num1 = cin.get();
+    sumOFNums = num1 + num2;
+    cout
