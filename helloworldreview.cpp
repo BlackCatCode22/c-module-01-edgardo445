38a4585ce725b717b949022d175e7cf463025118
edgardo445 <esaavedra1234556@gmail.com>
2025-08-13 11:44:17 -0700

    Add files via upload

---
+++ helloworldreview.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "HelloWorldReview";
+    cout << "Enter First Number: ";
+    cin >> num1;
+
+    cout << "Enter Second Number: ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+    cout << "The sum of the two numbers is: " << sum << endl;
+
+
+
+
+
+    return 0;
+
+}
