d820041c584c018bae207d235d2c6eac0352b802
edgardo445 <esaavedra1234556@gmail.com>
2025-08-18 12:12:20 -0700

    Add files via upload

---
+++ addTwoInts02.cpp
@@ -0,0 +1,62 @@
+// addTwoints.cpp
+    // dH 8/18/25
+
+    //References
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+int main() {
+    cout << "Hello and welcome to MY largest of three ints program. \n";
+
+    // Declare all variables needed.
+    int num1;
+    int num2;
+    int num3;
+    int sumofTheints;
+
+    // Initialize all my variables.
+    // assign a zero value to my variables.
+    num1 = 0;
+    num2 = 0;
+    num3 = 0;
+    sumofTheints = 0;
+
+
+
+    // Get three ints from the user
+    // Get num1
+    cout << "\n Please enter a value for num1: ";
+    cin >> num1;
+    cout << "\n Your entered " << num1 <<" for num1";
+    // Get num2
+    cout << "\n Please enter a value for num2: ";
+    cin >> num2;
+    cout << "\n Your entered " << num2 <<" for num2";
+    // Get num3
+    cout << "\n Please enter a value for num3: ";
+    cin >> num3;
+    cout << "\n Your entered " << num3 <<" for num3";
+
+    // Compare num1 and num2
+    if (num1 > num2) {
+        cout << num1 << " is greater than " << num2;
+    }   else
+        cout << num2 << " is greater than " << num1;
+    }
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
